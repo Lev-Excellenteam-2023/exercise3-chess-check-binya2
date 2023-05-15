737dea9851fe481fc68fb626f976f58a5d943884
beny shouraki <benyaminchor@gmail.com>
2023-05-15 13:37:33 +0300

    A test that verifies the locations the tool can go

--- chess_test.py
+++ chess_test.py
@@ -7,7 +7,7 @@ import chess_engine
 
 
 # unit tests
-def test_that_everything_is_free_around_him():
+def test_that_there_are_no_parts_to_eat_around():
     mock_game_state = Mock()
     mock_game_state.get_piece = lambda row, col: Player.EMPTY
     mock_game_state.is_valid_piece = lambda row, col: False
@@ -18,7 +18,7 @@ def test_that_everything_is_free_around_him():
 
     assert len(valid_moves) == 0
 
-def test_there_is_nothing_to_eat():
+def test_that_does_not_eat_itself():
     mock_game_state = Mock()
     mock_game_state.get_piece = lambda row, col: Piece.Rook('r', row, col, Player.PLAYER_2)
 
@@ -27,7 +27,6 @@ def test_there_is_nothing_to_eat():
     valid_moves = Piece.Knight.get_valid_piece_takes(mock_self_knight, mock_game_state)
     assert len(valid_moves) == 0
 
-
 def test_how_many_parts_to_eat_around():
     mock_game_state = Mock()
     mock_game_state.get_piece = lambda row, col: Piece.Rook('r', row, col, Player.PLAYER_1)
@@ -35,6 +34,20 @@ def test_how_many_parts_to_eat_around():
     mock_self_knight = Piece.Knight('n', 4, 4, Player.PLAYER_2)
 
     valid_moves = Piece.Knight.get_valid_piece_takes(mock_self_knight, mock_game_state)
+
+    assert len(valid_moves) == 8
+
+    expected_moves = [(2, 3), (2, 5), (3, 2), (3, 6), (5, 2), (5, 6), (6, 3), (6, 5)]
+    for move in expected_moves:
+        assert move in valid_moves
+
+def test_that_verifies_the_locations_the_tool_can_go_to():
+    mock_game_state = Mock()
+    mock_game_state.get_piece = lambda row, col: Player.EMPTY
+
+    mock_self_knight = Piece.Knight('n', 4, 4, Player.PLAYER_2)
+
+    valid_moves = Piece.Knight.get_valid_peaceful_moves(mock_self_knight, mock_game_state)
 
     assert len(valid_moves) == 8
 
