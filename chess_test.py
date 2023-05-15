62b6d6602f83048035a01baa1b814ec64f890bd9
beny shouraki <benyaminchor@gmail.com>
2023-05-15 13:43:50 +0300

    A test to get empty positions from the opponent's tools

--- chess_test.py
+++ chess_test.py
@@ -54,3 +54,14 @@ def test_that_verifies_the_locations_the_tool_can_go_to():
     expected_moves = [(2, 3), (2, 5), (3, 2), (3, 6), (5, 2), (5, 6), (6, 3), (6, 5)]
     for move in expected_moves:
         assert move in valid_moves
+
+
+def test_to_get_empty_positions_from_the_opponents_tools():
+    mock_game_state = Mock()
+    mock_game_state.get_piece = lambda row, col: Piece.Rook('r', row, col, Player.PLAYER_1)
+
+    mock_self_knight = Piece.Knight('n', 4, 4, Player.PLAYER_2)
+
+    valid_moves = Piece.Knight.get_valid_peaceful_moves(mock_self_knight, mock_game_state)
+
+    assert len(valid_moves) == 0
